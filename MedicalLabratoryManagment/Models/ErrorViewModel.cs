83c24276b6aaf07629e7df067cea0d2e9cbfa026
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-06-20 14:47:38 +0600

    Project created

---
+++ MedicalLabratoryManagment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MedicalLabratoryManagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
