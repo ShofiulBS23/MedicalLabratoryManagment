493ef97c9d1b0bae18b0729922cab6798c5e018d
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-06-21 18:35:43 +0600

    Main view implemented

---
+++ MedicalLabratoryManagment/Controllers/MainController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalLabratoryManagment.Controllers;
+
+public class MainController : Controller
+{
+    public MainController()
+    {
+        
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
